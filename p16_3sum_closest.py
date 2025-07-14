7f013bb96028a3a196e7817b241e7fae13e6f977
nathanansel28 <nathanansel.edu@gmail.com>
2025-07-14 16:51:52 +0800

    feat: solve leetcode no. 16, optimized version

--- p16_3sum_closest.py
+++ p16_3sum_closest.py
@@ -29,42 +29,27 @@ DEBUG=True
 logger=load_logger(DEBUG=DEBUG)
 from typing import List, Tuple
 
-
 class Solution:
     """2-pointer method."""
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         nums.sort()
-        logger.debug(f"nums sorted: {nums}")
-        best_sum = nums[0] + nums[1] + nums[2]
-        best_sum_idx = None
+        best_sum = None
 
         for i in range(len(nums) - 2):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue  
             j, k = i + 1, len(nums) - 1
-            logger.debug(f"\nITERATION {i, j, k}: {nums[i], nums[j], nums[k]}")
             while j < k:
 
                 current_sum = nums[i] + nums[j] + nums[k]
 
-                if abs(current_sum - target) < abs(best_sum - target):
-                    best_sum = current_sum
-                    best_sum_idx = [i, j, k]
-
-
-
-
-                if current_sum == target:
+                if current_sum - target == 0:
                     logger.debug(f"Found best best_sum from {nums[i], nums[j], nums[k]}")
                     return current_sum
-                elif current_sum < target: 
-                    logger.debug(f"Found current_sum < target from {nums[i], nums[j], nums[k]}")
-                    logger.debug(f"  changing {nums[j]} with {nums[j+1]}")
+                elif current_sum - target < 0: 
                     j += 1
                 else: 
-                    logger.debug(f"Found current_sum > target from {nums[i], nums[j], nums[k]}")
                     k -= 1
-                    logger.debug(f"  changing {nums[k]} with {nums[k-1]}")
 
                 if best_sum is None: 
                     best_sum = current_sum
@@ -72,12 +57,36 @@ class Solution:
 
                 elif abs(current_sum - target) < abs(best_sum - target):
                     best_sum = current_sum
-                    best_sum_idx = [i, j, k]
                     logger.debug(f"Found better best_sum from {nums[i], nums[j], nums[k]}")
-        if best_sum_idx is None: 
-            logger.debug("best_sum_idx is None")
-        else: 
-            logger.debug(f"\nFinal best sum: ijk={best_sum_idx} -> {nums[best_sum_idx[0]], nums[best_sum_idx[1]], nums[best_sum_idx[2]]}, {best_sum}")
+
+        return best_sum
+
+
+class Solution:
+    """2-pointer method, optimized"""
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        logger.debug(f"nums sorted: {nums}")
+        best_sum = nums[0] + nums[1] + nums[2]
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue  
+            j, k = i + 1, len(nums) - 1
+            logger.debug(f"\nITERATION {i, j, k}: {nums[i], nums[j], nums[k]}")
+            while j < k:
+                logger.debug(f"current ijk: {i,j,k}")
+                current_sum = nums[i] + nums[j] + nums[k]
+
+                if abs(current_sum - target) < abs(best_sum - target):
+                    best_sum = current_sum
+                elif current_sum < target:
+                    j += 1
+                elif current_sum > target: 
+                    k -= 1
+                else: 
+                    return current_sum
+
         return best_sum
 
         
